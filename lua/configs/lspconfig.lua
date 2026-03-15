9b7ff82144be101d02067017f5d118cf106e4a05
Falasw femi <femifalase228@gmail.com>
2026-03-15 18:43:57 +0100

    removed inlay from typescript and javascript

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -1,99 +1,108 @@
--- New style for nvim 0.11+ 
+-- New style for nvim 0.11+
 local on_attach = require("nvchad.configs.lspconfig").on_attach
 local capabilities = require("nvchad.configs.lspconfig").capabilities
 
 vim.lsp.config("cssls", {
-  on_attach = on_attach,
-  capabilities = capabilities,
+    on_attach = on_attach,
+    capabilities = capabilities,
 })
 
 vim.lsp.config("html", {
-  on_attach = on_attach,
-  capabilities = capabilities,
-  filetypes = { "html" },
+    on_attach = on_attach,
+    capabilities = capabilities,
+    filetypes = { "html" },
 })
 
 vim.lsp.config("gopls", {
-  on_attach = on_attach,
-  capabilities = capabilities,
-  cmd = { "gopls" },
-  filetypes = { "go", "gomod", "gowork", "gotmpl" },
-  settings = {
-    gopls = {
-      completeUnimported = true,
-      usePlaceholders = true,
-      analyses = {
-        unusedparams = true,
-      },
-      gofumpt = true,
+    on_attach = on_attach,
+    capabilities = capabilities,
+    cmd = { "gopls" },
+    filetypes = { "go", "gomod", "gowork", "gotmpl" },
+    settings = {
+        gopls = {
+            completeUnimported = true,
+            usePlaceholders = true,
+            analyses = {
+                unusedparams = true,
+            },
+            gofumpt = true,
+        },
     },
-  },
 })
 
 
 vim.lsp.config("ts_ls", {
-  on_attach = function(client, bufnr)
-    on_attach(client, bufnr)
-    if client:supports_method("textDocument/inlayHint") then
-      vim.lsp.inlay_hint.enable(true, { bufnr = bufnr })
-    end
-  end,
-  capabilities = capabilities,
-  settings = {
-    typescript = {
-      inlayHints = {
-        includeInlayParameterNameHints = "all",
-        includeInlayParameterNameHintsWhenArgumentMatchesName = true,
-        includeInlayFunctionParameterTypeHints = true,
-        includeInlayVariableTypeHints = true,
-        includeInlayPropertyDeclarationTypeHints = true,
-        includeInlayFunctionLikeReturnTypeHints = true,
-        includeInlayEnumMemberValueHints = true,
-      },
+    on_attach = function(client, bufnr)
+        on_attach(client, bufnr)
+        if client:supports_method("textDocument/inlayHint") then
+            -- vim.lsp.inlay_hint.enable(true, { bufnr = bufnr })
+        end
+    end,
+    capabilities = capabilities,
+    settings = {
+        typescript = {
+            inlayHints = {
+                includeInlayParameterNameHints = "none",
+                includeInlayParameterNameHintsWhenArgumentMatchesName = true,
+                includeInlayFunctionParameterTypeHints = true,
+                includeInlayVariableTypeHints = true,
+                includeInlayPropertyDeclarationTypeHints = true,
+                includeInlayFunctionLikeReturnTypeHints = true,
+                includeInlayEnumMemberValueHints = true,
+            },
+        },
+        javascript = {
+            inlayHints = {
+                includeInlayParameterNameHints = "none",
+                includeInlayParameterNameHintsWhenArgumentMatchesName = true,
+                includeInlayFunctionParameterTypeHints = true,
+                includeInlayVariableTypeHints = true,
+                includeInlayPropertyDeclarationTypeHints = true,
+                includeInlayFunctionLikeReturnTypeHints = true,
+                includeInlayEnumMemberValueHints = true,
+            },
+        },
     },
-    javascript = {
-      inlayHints = {
-        includeInlayParameterNameHints = "all",
-        includeInlayParameterNameHintsWhenArgumentMatchesName = true,
-        includeInlayFunctionParameterTypeHints = true,
-        includeInlayVariableTypeHints = true,
-        includeInlayPropertyDeclarationTypeHints = true,
-        includeInlayFunctionLikeReturnTypeHints = true,
-        includeInlayEnumMemberValueHints = true,
-      },
-    },
-  },
 })
 
 vim.lsp.config("tailwindcss", {
-  on_attach = on_attach,
-  capabilities = capabilities,
-  filetypes = {
-    "html", "css", "scss",
-    "javascript", "javascriptreact",
-    "typescript", "typescriptreact",
-  },
+    on_attach = on_attach,
+    capabilities = capabilities,
+    filetypes = {
+        "html", "css", "scss",
+        "javascript", "javascriptreact",
+        "typescript", "typescriptreact",
+    },
 })
 
 vim.lsp.config("emmet_language_server", {
-  on_attach = on_attach,
-  capabilities = capabilities,
-  filetypes = {
-    "html", "css",
-    "javascript", "javascriptreact",
-    "typescript", "typescriptreact",
-  },
+    on_attach = on_attach,
+    capabilities = capabilities,
+    filetypes = {
+        "html", "css",
+        "javascript", "javascriptreact",
+        "typescript", "typescriptreact",
+    },
+})
+
+vim.lsp.config("sqls", {
+    on_attach = on_attach,
+    capabilities = capabilities,
+    filetypes = {
+        "sql"
+    },
 })
 
 local servers = {
-  "cssls",
-  "html",
-  "gopls",
-  "ts_ls",
-  "tailwindcss",
-  "emmet_language_server",
+    "cssls",
+    "html",
+    "gopls",
+    "ts_ls",
+    "tailwindcss",
+    "emmet_language_server",
+    "sqls",
 }
 
 for _, server in ipairs(servers) do
-  vim.lsp.enable(server)
+    vim.lsp.enable(server)
 end
