1fffdb7b91d8ed9111b47f34ea86cbff68106103
falase femi <femifalase228@gmail.com>
2026-04-26 15:38:28 +0100

    chnage theme

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -4,7 +4,7 @@
 
 local options = {
     base46 = {
-        theme = "tokyonight",
+        theme = "cyberpunk-night",
         -- transparency = true,
         hl_override = {
             NvDashAscii = {
